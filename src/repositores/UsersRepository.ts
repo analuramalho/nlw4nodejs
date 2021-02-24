c17c9fd4c95e866c801086d804439854f33b3676
Ana Luiza Ramalho <analuizaramalho9@gmail.com>
2021-02-24 22:02:33 +0000

    refatora controller e cria repository

---
+++ src/repositores/UsersRepository.ts
@@ -0,0 +1,11 @@
+import { Repository } from 'typeorm'
+import { EntityRepository } from "typeorm";
+import{ User } from "../models/User"
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {
+
+
+}
+
+export { UsersRepository };
