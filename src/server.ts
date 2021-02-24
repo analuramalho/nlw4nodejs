4dcc5d45b23e5dc77bcacef6dd4508515893c510
Ana Luiza Ramalho <analuizaramalho9@gmail.com>
2021-02-24 02:44:19 +0000

    Cria funcionalidade de criação de usuario

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import 'reflect-metadata'
 import express from 'express'
 import "./database"
+import { router } from "./routes"
 
 const app  = express()
 
-app.get("/users", (req,res) =>{
-    return res.send("Hello o/")
-})
-
-app.post("/users",(req,res)=>{
-    return res.json({message:"Dados recebidos"})
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3000,()=>console.log("Server is running"))
